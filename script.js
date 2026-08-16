274d2836c7ba1dac903a38b464684853a4d22f41
merlinbigolive <merlinbigoagency@gmail.com>
2026-08-16 09:09:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-const observer=new IntersectionObserver(entries=>entries.forEach(entry=>{if(entry.isIntersecting)entry.target.classList.add("show")}),{threshold:.12});
-document.querySelectorAll(".reveal").forEach(el=>observer.observe(el));
